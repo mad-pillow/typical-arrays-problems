a99520fdea5c9b6c6aaa69d7a42594b22b6aface
Mad Pillow <dmtr.schv@gmail.com>
2021-02-07 00:19:22 +0200

    init: load solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    } else {
+        let minNumber = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (minNumber > array[i]) {
+                minNumber = array[i];
+            }
+        }
+        return minNumber;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    } else {
+        let maxNumber = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (maxNumber < array[i]) {
+                maxNumber = array[i];
+            }
+        }
+        return maxNumber;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    } else {
+        let sum = 0,
+            avrg = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        avrg = sum / array.length;
+        return avrg;
+    }
+};
